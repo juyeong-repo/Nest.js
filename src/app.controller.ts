30ff3dfeb814a488d63604d4433bd6d897a7bcdf
juyeong-repo <juyeong.park.tech@gmail.com>
2024-01-07 18:53:36 +0900

    test commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+interface Post {
+  author : string;
+  title : string;
+  content : string;
+  likecount : number;
+  commentCount : number;
+}
+
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPost(): Post {
+    return {
+      author : '작가',
+      title : '제목' ,
+      content : '내용' , 
+      likecount : 1 ,
+      commentCount : 9999,
+    };
   }
 }
